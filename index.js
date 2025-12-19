e90b7aa08f9b8e30b4e68a91b3e6204eb13e15ab
faqih28alam <faqih28alam@gmail.com>
2025-12-19 13:07:05 +0700

    feat: setup .env, gitignore, and improved folder structure

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ import bcrypt from 'bcrypt';                    // npm install bcrypt for encryp
 import hbs from 'hbs';                          // npm install hbs for handlebars
 import session from 'express-session';          // npm install express-session for session
 import flash from 'express-flash';              // npm install express-flash for flash message
-
+import 'dotenv/config';                         // Load variables from .env
 
 // SERVER SETUP
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;          // Use .env port or default to 3000
 
 // VIEW ENGINE CONFIGURATION (HBS)
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set('view engine', 'hbs');                                      // Set view engine to hbs
-app.set('views', path.join(__dirname, 'src/views'));                // Set views directory
-hbs.registerPartials(path.join(__dirname, 'src/views/partials'));   // Register partials directory
+app.set('view engine', 'hbs');                                          // Set view engine to hbs
+app.set('views', path.join(__dirname, 'src/views'));                    // Set views directory
+hbs.registerPartials(path.join(__dirname, 'src/views/partials'));       // Register partials directory
 
 // SERVE STATIC FILES
-app.use("/assets", express.static("src/assets"));                   // Serve static files from the "src/assets" directory
-app.use(express.urlencoded({ extended: false }));                   // Parse form data from POST requests 
+app.use('/assets', express.static(path.join(__dirname, 'src/assets'))); // Serve static files from the "src/assets" directory
+app.use(express.urlencoded({ extended: false }));                       // Parse form data from POST requests 
 
 
 // ROUTES
